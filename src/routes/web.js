1d2b5f56f32572445e4fc0aa63fae1f1b31b4557
Long Quang Trinh <trinhq011@gmail.com>
2023-07-14 15:50:20 +0700

    14/7

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getHomepage } from "../controller/homeController";
+import { getHomepage, getDetailpage } from "../controller/homeController";
 
 let router = express.Router();
 
 const initWebRoute = (app) => {
   router.get("/", getHomepage);
+  router.get("/detail/user/:userId", getDetailpage);
   router.get("/about", (req, res) => {
     res.send(`I'm Quang Long`);
   });
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import configViewEngine from "./configs/viewEngine";
 import initWebRoute from "./routes/web";
+//import connection from "./configs/connectDB";
 
 require("dotenv").config();
 
