c651e0c2cf4d3d636d2388ca23d1aaa2133221e7
Quang Long <trinhq011@gmail.com>
2023-07-19 22:25:56 +0700

    buoi 1

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,16 +1,1 @@
-import express from "express";
-import { getHomepage, getDetailpage } from "../controller/homeController";
 
-let router = express.Router();
-
-const initWebRoute = (app) => {
-  router.get("/", getHomepage);
-  router.get("/detail/user/:userId", getDetailpage);
-  router.get("/about", (req, res) => {
-    res.send(`I'm Quang Long`);
-  });
-
-  return app.use("/", router);
-};
-
-export default initWebRoute;
