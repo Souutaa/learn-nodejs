f0283daea81411c18f62b85707d11a731103ecd2
Quang Long <trinhq011@gmail.com>
2023-07-22 23:44:47 +0700

    migration, modal, seed

--- src/seeders/20230722095118-demo-user.js
+++ src/seeders/20230722095118-demo-user.js
@@ -2,12 +2,24 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
+  // email: DataTypes.STRING,
+  // password: DataTypes.STRING,
+  // firstName: DataTypes.STRING,
+  // lastName: DataTypes.STRING,
+  // address: DataTypes.STRING,
+  // gender: DataTypes.BOOLEAN,
+  // roleid: DataTypes.STRING,
   async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Users', [
       {
+        email: 'example@example.com',
+        password: '123456',
         firstName: 'John',
         lastName: 'Doe',
-        email: 'example@example.com',
+        address: 'TP.HCM',
+        gender: 1,
+        typeRole: 'ROLE',
+        keyRole: 'R1',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
